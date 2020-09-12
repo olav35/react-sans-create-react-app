c070604c049851ff273861787b4b1822238c1e3e
Olav Fosse <fosseolav@gmail.com>
2020-09-12 23:53:00 +0200

    Add mode-specific dummy constant BACKEND_URL

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
 const path = require('path')
+const webpack = require('webpack')
 
+const config = (env, argv) => {
+  const backend_url = argv.mode === 'production' ? 'example.com' : 'localhost:3001'
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'build'),
-    compress: true,
-    port: 3000
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-react']
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-react']
+          }
         }
-      }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
     ]
   }
 }
